ca07923cf39a9688a9fd0cf27d492d9e7fbdbc24
Yuji Iwai <iwai@sonicmoov.com>
2016-04-13 06:46:37 +0900

    bugfix: query string name

--- index.php
+++ index.php
@@ -7,10 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 
 $app->get('/callback', function (Request $request) use ($app) {
-  if ($request->query->get('hub.verify_token') == getenv('VALIDATION_TOKEN')) {
-    return new Response($request->query->get('hub.challenge'), 200);
+  if ($request->query->get('hub_verify_token') == getenv('VALIDATION_TOKEN')) {
+    return new Response($request->query->get('hub_challenge'), 200);
   }
-  return new Response($request->query->get('hub_verify_token'), 500);
   return new Response('Error, wrong validation token', 500);
 });
 
