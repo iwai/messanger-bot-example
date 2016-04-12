3456fe8f2234c3dc55b706404a6acb330d6ea036
Yuji Iwai <iwai@sonicmoov.com>
2016-04-13 06:42:04 +0900

    add debug code

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $app->get('/callback', function (Request $request) use ($app) {
   if ($request->query->get('hub.verify_token') == getenv('VALIDATION_TOKEN')) {
     return new Response($request->query->get('hub.challenge'), 200);
   }
-  return new Response($request->query->get('hub.verify_token'), 500);
+  return new Response($request->query->get('hub_verify_token'), 500);
   return new Response('Error, wrong validation token', 500);
 });
 
