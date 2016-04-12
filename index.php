d1c6d57872238287eb38f7b96a430cb25d4d4550
Yuji Iwai <iwai@sonicmoov.com>
2016-04-13 07:10:15 +0900

    Receive and Response

--- index.php
+++ index.php
@@ -13,4 +13,31 @@ $app->get('/callback', function (Request $request) use ($app) {
   return new Response('Error, wrong validation token', 500);
 });
 
+$app->post('/callback', function (Request $request) use ($app) {
+  $content = json_decode($request->getContent(), true);
+  $messaging_events = $content['entry'][0]['messaging'];
+
+  $client = new GuzzleHttp\Client();
+
+  foreach ($messaging_events as $event) {
+    try {
+      $client->request(
+        'POST', 'https://graph.facebook.com/v2.6/me/messages?access_token='.getenv('PAGE_ACCESS_TOKEN'), [
+            'body' => json_encode([
+              'recipient' => [ 'id' => $event['sender']['id'] ],
+              'message'   => [ 'text' => $event['message']['text'] ]
+            ]),
+            'headers' => [
+                'Content-Type' => 'application/json; charset=UTF-8',
+            ]
+        ]
+      );
+    } catch (Exception $e) {
+      error_log($e->getMessage());
+    }
+  }
+
+  return new Response('', 200);
+});
+
 $app->run();
