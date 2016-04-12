64160a6a7f4bbe0f9f3de7ae87f6a55577aa5030
Yuji Iwai <iwai@sonicmoov.com>
2016-04-13 06:09:06 +0900

    add debug code

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 
 $app->get('/callback', function (Request $request) use ($app) {
-  if ($request->get('hub.verify_token') === getenv('VALIDATION_TOKEN')) {
+  if ($request->get('hub.verify_token') == getenv('VALIDATION_TOKEN')) {
     return new Response($request->get('hub.challenge'), 200);
   }
-  return new Response('Error, wrong validation token', 500);
+  return new Response('Error, wrong validation token'.$request->get('hub.verify_token').getenv('VALIDATION_TOKEN'), 500);
 });
 
 $app->run();
